4a096233c8b71a4c7aa364f08ad93aaf7d3c3f2a
RouaSmisem02 <rouasmisem02@gmail.com>
2024-05-16 12:16:05 +0000

    solving Lab-18-2

--- src/components /Movie.jsx
+++ src/components /Movie.jsx
@@ -9,22 +9,22 @@ const Movie = ({ item, showModal }) => {
   };
 
   return (
-    <Card style={{ width: '18rem', marginBottom: '20px' }}>
+    <Card style={{ width: '18rem', marginBottom: '20px', backgroundColor: '#C0BEBE', fontFamily: 'Arial, sans-serif' }}>
       <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w185${item.posterPath}`} />
       <Card.Body>
         <Card.Title>{item.title}</Card.Title>
         {showDescription ? (
           <div>
             <Card.Text>{item.overview}</Card.Text>
-            <Button variant="link" onClick={toggleDescription}>View Less</Button>
+            <Button variant="link" onClick={toggleDescription} style={{ color: 'blue', fontWeight: 'bold' }}>View Less</Button>
           </div>
         ) : (
           <div>
             <Card.Text>{item.overview.substring(0, 100)}...</Card.Text>
-            <Button variant="link" onClick={toggleDescription}>View More</Button>
+            <Button variant="link" onClick={toggleDescription} style={{ color: 'blue', fontWeight: 'bold' }}>View More</Button>
           </div>
         )}
-        <Button variant="primary" onClick={() => showModal(item)} style={{ marginTop: '10px' }}>
+        <Button variant="primary" onClick={() => showModal(item)} style={{ marginTop: '10px', backgroundColor: 'white', color: 'blue', fontWeight: 'bold', width: '100%' }}>
           Add to Favorites
         </Button>
       </Card.Body>
