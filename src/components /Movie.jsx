193db377a7a39f2e791c4214075050f7d8547e63
RouaSmisem02 <162110860+RouaSmisem02@users.noreply.github.com>
2024-05-16 16:09:44 +0300

    Update Movie.jsx

--- src/components /Movie.jsx
+++ src/components /Movie.jsx
@@ -1,35 +1,52 @@
-import React, { useState } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Row, Col } from 'react-bootstrap';
+import axios from "axios";
+import Movie from "./Movie";
+import ModalMovie from "./ModalMovie";
 
-const Movie = ({ item, showModal }) => {
-  const [showDescription, setShowDescription] = useState(false);
+const MovieList = ({ moviesData }) => {
+    const [showModal, setShowModal] = useState(false);
+    const [clickedMovie, setClickedMovie] = useState({});
+    const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  const toggleDescription = () => {
-    setShowDescription(!showDescription);
-  };
+    useEffect(() => {
+        const fetchFavoriteMovies = async () => {
+            try {
+                const response = await axios.get("https://movie-management.onrender.com/getmovie");
+                setFavoriteMovies(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-  return (
-    <Card style={{ width: '18rem', marginBottom: '20px', backgroundColor: '#C0BEBE', fontFamily: 'Arial, sans-serif' }}>
-      <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w185${item.posterPath}`} />
-      <Card.Body>
-        <Card.Title>{item.title}</Card.Title>
-        {showDescription ? (
-          <div>
-            <Card.Text>{item.overview}</Card.Text>
-            <Button variant="link" onClick={toggleDescription} style={{ color: 'blue', fontWeight: 'bold' }}>View Less</Button>
-          </div>
-        ) : (
-          <div>
-            <Card.Text>{item.overview.substring(0, 100)}...</Card.Text>
-            <Button variant="link" onClick={toggleDescription} style={{ color: 'blue', fontWeight: 'bold' }}>View More</Button>
-          </div>
-        )}
-        <Button variant="primary" onClick={() => showModal(item)} style={{ marginTop: '10px', backgroundColor: 'white', color: 'blue', fontWeight: 'bold', width: '100%' }}>
-          Add to Favorites
-        </Button>
-      </Card.Body>
-    </Card>
-  );
+        fetchFavoriteMovies();
+    }, []);
+
+    const handleCloseModal = () => setShowModal(false);
+
+    const handleShowModal = (item) => {
+        setShowModal(true);
+        setClickedMovie(item);
+    };
+
+    return (
+        <>
+            <Row>
+                {moviesData.map(item => (
+                    <Col key={item.id}>
+                        <Movie item={item} showModal={handleShowModal} />
+                    </Col>
+                ))}
+            </Row>
+            <ModalMovie 
+                show={showModal} 
+                handleClose={handleCloseModal} 
+                clickedMovie={clickedMovie} 
+                favoriteMovies={favoriteMovies} 
+                setFavoriteMovies={setFavoriteMovies} 
+            />
+        </>
+    );
 };
 
-export default Movie;
+export default MovieList;
